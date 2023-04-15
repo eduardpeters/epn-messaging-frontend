ca58a93bef5f7b3d7420004be205a37f20fcab56
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-04-15 11:00:55 +0200

    Add basic socket connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import './App.css'
+import { io } from 'socket.io-client';
+import './App.css';
+
+const socket = io(import.meta.env.VITE_API_BASE_URL);
 
 function App() {
-
     return (
         <div className="App">
             <h1>My first Vite App</h1>
