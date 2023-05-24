7e7bb58c7d17c9d8eac8833f131f1d659b3a3702
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-05-24 20:43:06 +0200

    Add Home description@

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,6 +6,10 @@ const Home = () => {
     return (
         <main className="home__container">
             <h1 className="home__title">Welcome to EPN Messenger</h1>
+            <div className="home__description">
+                <h3 className="description__text description__text-left">A Web App for instant messaging</h3>
+                <h3 className="description__text description__text-right">Join the group chat, create an account!</h3>
+            </div>
             <div className="link__container">
                 <Link to="/login" className="home__link">Log In</Link>
                 <Link to="/register" className="home__link">Register!</Link>
